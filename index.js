0bab240dcd1c4e73b35c8692a48c2368d383703c
GreenColoredLife <greencoloredlife@gmail.com>
2020-03-08 01:13:38 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const middleware = require('@line/bot-sdk').middleware
+const JSONParseError = require('@line/bot-sdk').JSONParseError
+const SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed
+
+const app = express()
+
+const config = {
+  channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
+  channelSecret: 'YOUR_CHANNEL_SECRET'
+}
+
+app.use(middleware(config))
+
+app.post('/webhook', (req, res) => {
+  res.json(req.body.events) // req.body will be webhook event object
+})
+
+app.use((err, req, res, next) => {
+  if (err instanceof SignatureValidationFailed) {
+    res.status(401).send(err.signature)
+    return
+  } else if (err instanceof JSONParseError) {
+    res.status(400).send(err.raw)
+    return
+  }
+  next(err) // will throw default 500
+})
+
+app.listen(8080)
