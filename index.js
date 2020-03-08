ff53ccc5dce5f7c426e5115475edfc6030e0a574
GreenColoredLife <greencoloredlife@gmail.com>
2020-03-08 10:54:38 +0700

    new stuff

--- index.js
+++ index.js
@@ -19,7 +19,18 @@ app.post("/webhook", (req, res) => {
     res.json(req.body.events[0]); // req.body will be webhook event object
     const client = new line.Client(config);
 
-    const message = {         
+    client
+        .getProfile(req.body.events[0].source.userId)
+        .then(profile => {
+            console.log(profile.displayName);
+            console.log(profile.userId);
+            console.log(profile.statusMessage);
+        })
+        .catch(err => {
+            // error handling
+        });
+
+    const message = {
         type: "text",
         text: "You just said " + req.body.events[0].message.text + "!"
     };
