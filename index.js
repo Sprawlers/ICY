6a651331e0ee473a8a25aaa7a810de1b9296ac50
GreenColoredLife <greencoloredlife@gmail.com>
2020-03-08 10:36:39 +0700

    changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const line = require("@line/bot-sdk");
 const middleware = require("@line/bot-sdk").middleware;
 const JSONParseError = require("@line/bot-sdk").JSONParseError;
 const SignatureValidationFailed = require("@line/bot-sdk")
