ff36b1d2ce43d1bc2dda47b0cc3062fe9e76af51
GreenColoredLife <greencoloredlife@gmail.com>
2020-03-24 21:35:09 +0700

    Changing to gitlab

--- v3.sh
+++
@@ -1,7 +0,0 @@
-curl -v -X POST https://api-data.line.me/v2/bot/richmenu/richmenu-ac7437a29279eb4b4b85b2920947315b/content \
--H "Authorization: Bearer HOqcUq5EL4moJ6XL5BDfnrlPKtVzwPBtvA3q8ULZoetFya2BT3Sqzl00W4xIz7naewbcBlvGqyhghPw9SxVLWw4Cd7/bzRWS2pvuU9DrXHi6FTgseJrpcIm7nvg+0aPPTkrGfkUTHxaN39mVeDNZ4wdB04t89/1O/w1cDnyilFU=" \
--H "Content-Type: image/png" \
--T v7.png
-
-
-richmenu-ac7437a29279eb4b4b85b2920947315b
