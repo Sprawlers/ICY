9a497f47771d80c7396e3c9104854fcff01b7366
bosswt <boss.wt13@gmail.com>
2020-03-27 17:39:38 +0700

    add carousel

--- index.js
+++ index.js
@@ -42,7 +42,187 @@ app.post('/webhook', (req, res) => {
   //Function Location
   function homework(agent) {
     agent.add('Please select a subject...')
-    agent.add('##########')
+    const payloadJSON = {
+      type: 'flex',
+      altText: 'this is a flex message',
+      contents: {
+        type: 'carousel',
+        contents: [
+          {
+            type: 'bubble',
+            direction: 'ltr',
+            header: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: 'ComProg',
+                  weight: 'bold',
+                  size: 'xxl',
+                  align: 'center',
+                  gravity: 'center',
+                  color: '#000000',
+                  wrap: true
+                }
+              ],
+              backgroundColor: '#FFFFFF',
+              cornerRadius: '0px'
+            },
+            hero: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: '📅 Deadline April 7',
+                  size: 'lg',
+                  align: 'center',
+                  gravity: 'center',
+                  contents: [
+                    {
+                      type: 'span',
+                      text: '📅 Deadline: '
+                    },
+                    {
+                      type: 'span',
+                      text: 'April 7',
+                      weight: 'regular'
+                    }
+                  ]
+                }
+              ]
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'separator'
+                }
+              ]
+            },
+            footer: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'button',
+                  action: {
+                    type: 'uri',
+                    uri: 'http://linecorp.com/',
+                    label: 'View Solution'
+                  },
+                  gravity: 'center',
+                  style: 'secondary'
+                }
+              ],
+              backgroundColor: '#FFFFFF'
+            },
+            size: 'kilo',
+            styles: {
+              header: {
+                backgroundColor: '#ffaaaa',
+                separator: false
+              },
+              body: {
+                backgroundColor: '#ffffff'
+              },
+              footer: {
+                backgroundColor: '#aaaaff'
+              }
+            }
+          },
+          {
+            type: 'bubble',
+            direction: 'ltr',
+            header: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: 'Calculus',
+                  weight: 'bold',
+                  size: 'xxl',
+                  align: 'center',
+                  gravity: 'center',
+                  color: '#000000',
+                  wrap: true
+                }
+              ],
+              backgroundColor: '#FFFFFF',
+              cornerRadius: '0px'
+            },
+            hero: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: '📅 Deadline April 7',
+                  size: 'lg',
+                  align: 'center',
+                  gravity: 'center',
+                  contents: [
+                    {
+                      type: 'span',
+                      text: '📅 Deadline: '
+                    },
+                    {
+                      type: 'span',
+                      text: 'April 7',
+                      weight: 'regular'
+                    }
+                  ]
+                }
+              ]
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'separator'
+                }
+              ]
+            },
+            footer: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'button',
+                  action: {
+                    type: 'uri',
+                    uri: 'http://linecorp.com/',
+                    label: 'View Solution'
+                  },
+                  gravity: 'center',
+                  style: 'secondary'
+                }
+              ],
+              backgroundColor: '#FFFFFF'
+            },
+            size: 'kilo',
+            styles: {
+              header: {
+                backgroundColor: '#ffaaaa',
+                separator: false
+              },
+              body: {
+                backgroundColor: '#ffffff'
+              },
+              footer: {
+                backgroundColor: '#aaaaff'
+              }
+            }
+          }
+        ]
+      }
+    }
+    let payload = new Payload(`LINE`, payloadJSON, { sendAsMessage: true })
+    agent.add(payload)
   }
 
   // Run the proper function handler based on the matched Dialogflow intent name
