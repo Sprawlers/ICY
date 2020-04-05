27cf0bc7d5313a8088b48c89ea0a5dbe98ade410
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-05 16:01:02 +0700

    Testing out new JSON function

---
+++ src/functions.js
@@ -0,0 +1,114 @@
+/**
+ * a function that constructs a carousel message for homework
+ * @param obj: an object containing objects of each subject's title, deadline, and link
+ *
+ * Example obj:
+ * {
+ *     "Calculus": {
+ *         "deadline": (DateTime Object),
+ *         "link": (URL)
+ *     }
+ * }
+ *
+ */
+export const generateHomework = obj => ({
+    "type": "flex",
+    "altText": "homework",
+    "contents": {
+        "type": "carousel",
+        "contents": generateBubbles(obj)
+    }
+});
+
+const getDeadlineFromObject = dateTimeObject => {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    return `${monthNames[dateTimeObject.getMonth()]} + ${dateTimeObject.getDate()}`;
+};
+
+const generateBubbles = obj => Object.keys(obj).map(key => ({
+    "type": "bubble",
+    "direction": "ltr",
+    "header": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "text",
+                "text": key,
+                "weight": "bold",
+                "size": "xxl",
+                "align": "center",
+                "gravity": "center",
+                "color": "#000000",
+                "wrap": true
+            }
+        ],
+        "backgroundColor": "#FFFFFF",
+        "cornerRadius": "0px"
+    },
+    "hero": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "text",
+                "text": `📅 Deadline ${getDeadlineFromObject(obj[key]["deadline"])}`,
+                "size": "lg",
+                "align": "center",
+                "gravity": "center",
+                "contents": [
+                    {
+                        "type": "span",
+                        "text": "📅 Deadline: "
+                    },
+                    {
+                        "type": "span",
+                        "text": getDeadlineFromObject(obj[key]["deadline"]),
+                        "weight": "regular"
+                    }
+                ]
+            }
+        ]
+    },
+    "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "separator"
+            }
+        ]
+    },
+    "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "button",
+                "action": {
+                    "type": "uri",
+                    "uri": obj[key]["url"],
+                    "label": "View Solution"
+                },
+                "gravity": "center",
+                "style": "secondary"
+            }
+        ],
+        "backgroundColor": "#FFFFFF"
+    },
+    "size": "kilo",
+    "styles": {
+        "header": {
+            "backgroundColor": "#ffaaaa",
+            "separator": false
+        },
+        "body": {
+            "backgroundColor": "#ffffff"
+        },
+        "footer": {
+            "backgroundColor": "#aaaaff"
+        }
+    }
+}));
--- src/index.js
+++ src/index.js
@@ -5,231 +5,63 @@ const morgan = require('morgan')
 const port = process.env.PORT || 4000
 
 // Import the appropriate class
-const { WebhookClient, Payload } = require('dialogflow-fulfillment')
+const {WebhookClient, Payload} = require('dialogflow-fulfillment')
+const {generateHomework} = require('./functions.js')
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send({
-    success: true
-  })
+    res.send({
+        success: true
+    })
 })
 
 app.get('/test', (req, res) => {
-  res.send('Hello Test')
-  console.log('Hi There')
+    res.send('Hello Test')
+    console.log('Hi There')
 })
 
 app.post('/webhook', (req, res) => {
-  console.log('POST: /')
-  console.log('Body: ', req.body)
+    console.log('POST: /')
+    console.log('Body: ', req.body)
 
-  //Create an instance
-  const agent = new WebhookClient({
-    request: req,
-    response: res
-  })
+    //Create an instance
+    const agent = new WebhookClient({
+        request: req,
+        response: res
+    })
 
-  //Test get value of WebhookClient
-  console.log('agentVersion: ' + agent.agentVersion)
-  console.log('intent: ' + agent.intent)
-  console.log('locale: ' + agent.locale)
-  console.log('query: ', agent.query)
-  console.log('session: ', agent.session)
+    //Test get value of WebhookClient
+    console.log('agentVersion: ' + agent.agentVersion)
+    console.log('intent: ' + agent.intent)
+    console.log('locale: ' + agent.locale)
+    console.log('query: ', agent.query)
+    console.log('session: ', agent.session)
 
-  //Function Location
-  function homework(agent) {
-    agent.add('Please select a subject...')
-    const payloadJSON = {
-      type: 'flex',
-      altText: 'this is a flex message',
-      contents: {
-        type: 'carousel',
-        contents: [
-          {
-            type: 'bubble',
-            direction: 'ltr',
-            header: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'text',
-                  text: 'ComProg',
-                  weight: 'bold',
-                  size: 'xxl',
-                  align: 'center',
-                  gravity: 'center',
-                  color: '#000000',
-                  wrap: true
-                }
-              ],
-              backgroundColor: '#FFFFFF',
-              cornerRadius: '0px'
+    //Function Location
+    function homework(agent) {
+        agent.add('Please select a subject...')
+        const payloadJSON = generateHomework({
+            "Calculus": {
+                "deadline": new Date(),
+                "link": "google.com"
             },
-            hero: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'text',
-                  text: '📅 Deadline April 7',
-                  size: 'lg',
-                  align: 'center',
-                  gravity: 'center',
-                  contents: [
-                    {
-                      type: 'span',
-                      text: '📅 Deadline: '
-                    },
-                    {
-                      type: 'span',
-                      text: 'April 7',
-                      weight: 'regular'
-                    }
-                  ]
-                }
-              ]
-            },
-            body: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'separator'
-                }
-              ]
-            },
-            footer: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'button',
-                  action: {
-                    type: 'uri',
-                    uri: 'http://linecorp.com/',
-                    label: 'View Solution'
-                  },
-                  gravity: 'center',
-                  style: 'secondary'
-                }
-              ],
-              backgroundColor: '#FFFFFF'
-            },
-            size: 'kilo',
-            styles: {
-              header: {
-                backgroundColor: '#ffaaaa',
-                separator: false
-              },
-              body: {
-                backgroundColor: '#ffffff'
-              },
-              footer: {
-                backgroundColor: '#aaaaff'
-              }
+            "Phys": {
+                "deadline": new Date(),
+                "link": "google.com"
             }
-          },
-          {
-            type: 'bubble',
-            direction: 'ltr',
-            header: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'text',
-                  text: 'Calculus',
-                  weight: 'bold',
-                  size: 'xxl',
-                  align: 'center',
-                  gravity: 'center',
-                  color: '#000000',
-                  wrap: true
-                }
-              ],
-              backgroundColor: '#FFFFFF',
-              cornerRadius: '0px'
-            },
-            hero: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'text',
-                  text: '📅 Deadline April 7',
-                  size: 'lg',
-                  align: 'center',
-                  gravity: 'center',
-                  contents: [
-                    {
-                      type: 'span',
-                      text: '📅 Deadline: '
-                    },
-                    {
-                      type: 'span',
-                      text: 'April 7',
-                      weight: 'regular'
-                    }
-                  ]
-                }
-              ]
-            },
-            body: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'separator'
-                }
-              ]
-            },
-            footer: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'button',
-                  action: {
-                    type: 'uri',
-                    uri: 'http://linecorp.com/',
-                    label: 'View Solution'
-                  },
-                  gravity: 'center',
-                  style: 'secondary'
-                }
-              ],
-              backgroundColor: '#FFFFFF'
-            },
-            size: 'kilo',
-            styles: {
-              header: {
-                backgroundColor: '#ffaaaa',
-                separator: false
-              },
-              body: {
-                backgroundColor: '#ffffff'
-              },
-              footer: {
-                backgroundColor: '#aaaaff'
-              }
-            }
-          }
-        ]
-      }
+        })
+        payload = new Payload(`LINE`, payloadJSON, {sendAsMessage: true})
+        agent.add(payload)
     }
-    let payload = new Payload(`LINE`, payloadJSON, { sendAsMessage: true })
-    agent.add(payload)
-  }
 
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map()
-  intentMap.set('Homework', homework) // "Homework" is once Intent Name of Dialogflow Agent
-  agent.handleRequest(intentMap)
+    // Run the proper function handler based on the matched Dialogflow intent name
+    let intentMap = new Map()
+    intentMap.set('Homework', homework) // "Homework" is once Intent Name of Dialogflow Agent
+    agent.handleRequest(intentMap)
 })
 
 app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`)
+    console.log(`Server is running at port: ${port}`)
 })
