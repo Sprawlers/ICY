fea737eecab6f1cc0bdc4e7e947ad5cc5f6655c9
bosswt <boss.wt13@gmail.com>
2020-04-06 00:45:52 +0700

    Richmenu Creator Update

--- src/richmenu/richmenu_creator.js
+++ src/richmenu/richmenu_creator.js
@@ -1,10 +1,14 @@
 const newman = require('newman') // require newman in your project
 var data = require('./richmenu_creator.json')
+var richmenu = require('./default/richmenu.json')
 // call newman.run to pass `options` object and wait for callback
 var str = JSON.stringify(data)
+var strrich = JSON.stringify(richmenu)
 var path = process.cwd()
 path += '/src/richmenu/default/richmenu.jpg'
 str = str.replace('%PATH%', path)
+strrich = strrich.replace(/"/g, '\\"')
+str = str.replace('%RICHMENU%', strrich)
 const obj = JSON.parse(str)
 newman.run(
   {
