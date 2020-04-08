e4eb54492f2e26521f5c143f8d90b8221c93520e
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-08 13:53:19 +0700

    removed email address

--- src/model/schema/User.js
+++ src/model/schema/User.js
@@ -6,14 +6,12 @@ const Schema = mongoose.Schema
  *
  * userID: UUID of the user
  * profileName: profile name of the user as it appears in Line
- * email: email address of the user
  * isAdmin: check whether the user is an admin or not
  *
  */
 const userSchema = new Schema({
     userID: String,
     profileName: String,
-    email: String,
     isAdmin: Boolean,
 })
 
