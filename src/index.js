dacb7b7801558001aaee40befc2473f1a11e9cff
bosswt <boss.wt13@gmail.com>
2020-04-08 15:00:00 +0700

    Integrated dialogflow to index.js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,89 @@
 const express = require('express')
-const app = express()
+const config = require('./config')
+const line = require('@line/bot-sdk')
 const bodyParser = require('body-parser')
+const dialogflow = require('dialogflow').v2beta1
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const port = process.env.PORT || 4000
-
-// Import environmental variables
-require('dotenv').config({ path: '../.env' })
+const projectId = config.projectId
+const app = express()
 
 // Import the appropriate class
-const { WebhookClient, Payload } = require('dialogflow-fulfillment')
 const { generateHomework } = require('./controller/functions.js')
 
 // Import database functions
 const { hw } = require('./model/functions')
-
-app.use(morgan('dev'))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
 
-// Connect to database
-const mongoDB = process.env.DB_HOST
+const mongoDB = config.db_host
 const db = mongoose
   .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-  .then(console.log('Connected to database'))
-  .catch((err) => console.error(err))
+  .then(console.log('Connected to database')).cat
 
 app.get('/', (req, res) => {
   res.send({
     success: true,
   })
 })
-
-app.post('/webhook', (req, res) => {
-  console.log('POST: /')
-  console.log('Body: ', req.body)
-  //Create an instance
-  const agent = new WebhookClient({
-    request: req,
-    response: res,
-  })
-
-  //Test get value of WebhookClient
-  console.log('agentVersion: ' + agent.agentVersion)
-  console.log('intent: ' + agent.intent)
-  console.log('locale: ' + agent.locale)
-  console.log('query: ', agent.query)
-  console.log('session: ', agent.session)
-
-  //Function Location
-  async function homework(agent) {
-    agent.add('Please select a subject...')
-    const homeworkObjectArr = await hw();
-    const payloadJSON = generateHomework(homeworkObjectArr)
-    let payload = new Payload(`LINE`, payloadJSON, { sendAsMessage: true })
-    agent.add(payload)
-  }
-
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map()
-  intentMap.set('Homework', homework) // "Homework" is once Intent Name of Dialogflow Agent
-  agent.handleRequest(intentMap).catch(e => console.error(e))
+const sessionClient = new dialogflow.SessionsClient({
+  keyFilename: `../${config.filename}`,
 })
 
-app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`)
+app.post('/webhook', async (req, res) => {
+  res.json(req.body.events[0]) // req.body will be webhook event object
+  const client = new line.Client(config.line)
+  const replyMsg = {
+    type: 'text',
+    text: null,
+  }
+  const event = req.body.events[0]
+  const userId = event.source.userId
+  const userMsg = event.message.text
+  const replyToken = event.replyToken
+  const profile = await client.getProfile(event.source.userId)
+  console.log(`User: ${profile.displayName}`)
+  console.log(userMsg)
+  console.log(replyToken)
+  const intentResponse = await detectIntent(userId, userMsg, 'en-US')
+  console.log(intentResponse)
+  const query = intentResponse.queryResult
+  const intent = query.intent.displayName
+  switch (intent) {
+    case 'Homework':
+      console.log(`Intent ${intent}`)
+      replyMsg.text = 'Please select a subject...'
+      const homeworkObjectArr = await hw()
+      const payloadJSON = generateHomework(homeworkObjectArr)
+      console.log(payloadJSON)
+      await client.replyMessage(replyToken, payloadJSON)
+      break
+    default:
+      console.log(`Intent ${intent}`)
+      replyMsg.text = query.fulfillmentText
+      await client.replyMessage(replyToken, replyMsg)
+      break
+  }
+  res.status(200).end()
+})
+
+const detectIntent = async (userId, message, languageCode) => {
+  console.log(projectId, userId)
+  const sessionPath = sessionClient.sessionPath(projectId, userId)
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: message,
+        languageCode: languageCode,
+      },
+    },
+  }
+  const responses = await sessionClient.detectIntent(request)
+  return responses[0]
+}
+
+app.listen(config.port, () => {
+  console.log(`Server is running at port ${config.port}`)
 })
