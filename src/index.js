0bcd49f23c65c3c669b80de1a52aba2b108ac06b
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-08 15:22:21 +0700

    Trying out database user feature

--- src/index.js
+++ src/index.js
@@ -9,81 +9,83 @@ const projectId = config.projectId
 const app = express()
 
 // Import the appropriate class
-const { generateHomework } = require('./controller/functions.js')
+const {generateHomework} = require('./controller/functions.js')
 
 // Import database functions
-const { hw } = require('./model/functions')
+const {hw, getUserByID} = require('./model/functions')
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(morgan('dev'))
 
 const mongoDB = config.db_host
 const db = mongoose
-  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-  .then(console.log('Connected to database')).cat
+    .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(console.log('Connected to database')).cat
 
 app.get('/', (req, res) => {
-  res.send({
-    success: true,
-  })
+    res.send({
+        success: true,
+    })
 })
 const sessionClient = new dialogflow.SessionsClient({
-  keyFilename: `../${config.filename}`,
+    keyFilename: `../${config.filename}`,
 })
 
 app.post('/webhook', async (req, res) => {
-  res.json(req.body.events[0]) // req.body will be webhook event object
-  const client = new line.Client(config.line)
-  const replyMsg = {
-    type: 'text',
-    text: null,
-  }
-  const event = req.body.events[0]
-  const userId = event.source.userId
-  const userMsg = event.message.text
-  const replyToken = event.replyToken
-  const profile = await client.getProfile(event.source.userId)
-  console.log(`User: ${profile.displayName}`)
-  console.log(userMsg)
-  console.log(replyToken)
-  const intentResponse = await detectIntent(userId, userMsg, 'en-US')
-  console.log(intentResponse)
-  const query = intentResponse.queryResult
-  const intent = query.intent.displayName
-  switch (intent) {
-    case 'Homework':
-      console.log(`Intent ${intent}`)
-      replyMsg.text = 'Please select a subject...'
-      const homeworkObjectArr = await hw()
-      const payloadJSON = generateHomework(homeworkObjectArr)
-      console.log(payloadJSON)
-      await client.replyMessage(replyToken, payloadJSON)
-      break
-    default:
-      console.log(`Intent ${intent}`)
-      replyMsg.text = query.fulfillmentText
-      await client.replyMessage(replyToken, replyMsg)
-      break
-  }
-  res.status(200).end()
+    res.json(req.body.events[0]) // req.body will be webhook event object
+    const client = new line.Client(config.line)
+    const replyMsg = {
+        type: 'text',
+        text: null,
+    }
+    const event = req.body.events[0]
+    const userID = event.source.userID
+    const userMsg = event.message.text
+    const replyToken = event.replyToken
+    const profile = await client.getProfile(event.source.userID)
+    console.log(`User: ${profile.displayName}`)
+    console.log(userMsg)
+    console.log(replyToken)
+    const intentResponse = await detectIntent(userID, userMsg, 'en-US')
+    console.log(intentResponse)
+    console.log("DEBUG: Checking if user exists...")
+    const userObject = getUserByID(userID)
+    console.log(userObject)
+    const query = intentResponse.queryResult
+    const intent = query.intent.displayName
+    switch (intent) {
+        case 'Homework':
+            console.log(`Intent ${intent}`)
+            const homeworkObjectArr = await hw()
+            const payloadJSON = generateHomework(homeworkObjectArr)
+            console.log(payloadJSON)
+            await client.replyMessage(replyToken, payloadJSON)
+            break
+        default:
+            console.log(`Intent ${intent}`)
+            replyMsg.text = query.fulfillmentText
+            await client.replyMessage(replyToken, replyMsg)
+            break
+    }
+    res.status(200).end()
 })
 
-const detectIntent = async (userId, message, languageCode) => {
-  console.log(projectId, userId)
-  const sessionPath = sessionClient.sessionPath(projectId, userId)
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: message,
-        languageCode: languageCode,
-      },
-    },
-  }
-  const responses = await sessionClient.detectIntent(request)
-  return responses[0]
+const detectIntent = async (userID, message, languageCode) => {
+    console.log(projectId, userID)
+    const sessionPath = sessionClient.sessionPath(projectId, userID)
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: languageCode,
+            },
+        },
+    }
+    const responses = await sessionClient.detectIntent(request)
+    return responses[0]
 }
 
 app.listen(config.port, () => {
-  console.log(`Server is running at port ${config.port}`)
+    console.log(`Server is running at port ${config.port}`)
 })
--- src/model/functions.js
+++ src/model/functions.js
@@ -1,10 +1,17 @@
 const Homework = require('./schema/Homework')
+const User = require('./schema/User')
 
 // Gets all homework documents, called with hw()
 function getAllHomework() {
   return Homework.find({})
 }
 
+// Gets a user by UUID
+function getUserByID(userID) {
+  return User.find({ userID })
+}
+
 module.exports = {
   hw: getAllHomework,
+  getUserByID
 }
