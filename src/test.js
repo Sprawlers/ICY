8cf19231f0c35d6a10ed838d8ba2d796d711508e
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-10 11:55:37 +0700

    fix

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
 // WARNING: USE THIS JS FILE FOR TESTING ONLY
 const {downloadPDFFromURL, removeFile} = require('./controller/functions')
 
-removeFile('./Dummy.pdf').then(() => console.log("successfully removed file")).catch(e => console.error(e))
+removeFile('./Dummy.pdf').catch(e => console.error(e))
