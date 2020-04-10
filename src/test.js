749b825616eab3920233a28475f9786283bf50ff
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-10 12:14:32 +0700

    added watermark function and testing it

--- src/test.js
+++ src/test.js
@@ -1,4 +1,7 @@
 // WARNING: USE THIS JS FILE FOR TESTING ONLY
-const {downloadPDFFromURL, removeFile} = require('./controller/functions')
+const {downloadPDFFromURL, removeFile, watermarkFile} = require('./controller/functions')
 
-removeFile('./test.txt')
+downloadPDFFromURL('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 'Dummy.pdf').catch(e => console.error(e))
+
+watermarkFile(__dirname + '/Dummy.pdf', 'ICY')
+
