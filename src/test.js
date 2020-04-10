730962adbba8a67dedb5094d84f9fe72a0283d6b
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-10 12:16:10 +0700

    fixing

--- src/test.js
+++ src/test.js
@@ -1,7 +1,9 @@
 // WARNING: USE THIS JS FILE FOR TESTING ONLY
 const {downloadPDFFromURL, removeFile, watermarkFile} = require('./controller/functions')
 
-downloadPDFFromURL('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 'Dummy.pdf').catch(e => console.error(e))
+downloadPDFFromURL('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 'Dummy.pdf')
+    .then(() => watermarkFile(__dirname + '/Dummy.pdf', 'ICY'))
+    .catch(e => console.error(e))
 
-watermarkFile(__dirname + '/Dummy.pdf', 'ICY')
+
 
