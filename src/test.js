d195547ac90fc13d95426d204db80cc49b06af67
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-10 12:47:13 +0700

    edited path

--- src/test.js
+++ src/test.js
@@ -2,7 +2,7 @@
 const {downloadPDFFromURL, removeFile, watermarkFile} = require('./controller/functions')
 
 downloadPDFFromURL('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 'Dummy.pdf')
-    .then(() => watermarkFile('ICY/src/Dummy.pdf', 'ICY'))
+    .then(() => watermarkFile('/ICY/src/Dummy.pdf', 'ICY'))
     .catch(e => console.error(e))
 
 
