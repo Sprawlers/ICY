58bfac968abbb706a51268b0948c33db196747a8
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-10 11:19:28 +0700

    minor typo

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
 const {downloadPDFFromURL} = require('./controller/functions')
 
-downloadPDFFromURL('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 'Dummy')
+downloadPDFFromURL('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 'Dummy.pdf')
     .then(output => console.log(output)).catch(e => console.error(e))
