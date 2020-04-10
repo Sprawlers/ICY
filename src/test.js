11503558285a1e03941b16de8cb9f40adde65695
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-10 11:56:22 +0700

    fix again;

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
 // WARNING: USE THIS JS FILE FOR TESTING ONLY
 const {downloadPDFFromURL, removeFile} = require('./controller/functions')
 
-removeFile('./Dummy.pdf').catch(e => console.error(e))
+removeFile('./Testing.pdf')
