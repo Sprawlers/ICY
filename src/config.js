6f723cdc83be09bdf05c37dfad3bb573a67e3c23
bosswt <boss.wt13@gmail.com>
2020-04-11 20:53:46 +0700

    Authentication config dialogflow

--- src/config.js
+++ src/config.js
@@ -6,6 +6,8 @@ const envFound = dotenv.config({ path: '../.env' })
 if (!envFound) console.log("⚠️  Couldn't find .env file")
 
 module.exports = {
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
   bitly_token: process.env.BITLY_TOKEN,
   webhookid: process.env.WEBHOOK_ID,
   filename: process.env.FILE_NAME,
