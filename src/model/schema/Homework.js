8c3684f8707f11cf7342374ac37c06292b4be527
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-11 12:06:08 +0700

    edited Homework schema (part of redefining homework)

--- src/model/schema/Homework.js
+++ src/model/schema/Homework.js
@@ -5,17 +5,15 @@ const Schema = mongoose.Schema
  * Homework Model
  *
  * title: name of the subject
- * deadline: due date of the latest assignment
- * links: object containing urls to different homework assignments
+ * assignments: object containing assignments with their name, deadline, and link
  *
  */
 const homeworkSchema = new Schema(
-  {
-    title: String,
-    deadline: Date,
-    links: Object,
-  },
-  { versionKey: false }
+    {
+        title: String,
+        assignments: Object
+    },
+    {versionKey: false}
 )
 
 // Prepares schema as a model
