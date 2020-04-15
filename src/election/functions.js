86acbff9c1b1299f83a97f75d55190695587abfc
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-15 23:18:52 +0700

    finished function

--- src/election/functions.js
+++ src/election/functions.js
@@ -1,6 +1,6 @@
 const processDataForGraph = arr =>
     arr.map(voter => ({timestamp: voter["createdAt"]})).map((time, count) => ({
-        x: new Date(time),
+        x: (new Date(time)).toString(),
         y: count + 1
     }))
 
