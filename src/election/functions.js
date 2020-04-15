3ec3c2cfdf380479ee965b274cd0ded6810a8e52
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-16 03:50:16 +0700

    fix

--- src/election/functions.js
+++ src/election/functions.js
@@ -1,6 +1,5 @@
 const processDataForGraph = arr =>
     arr.map(voter => voter["createdAt"]).map((time, count) => {
-        console.log(time);
         return ({
             x: new Date(time),
             y: count + 1
