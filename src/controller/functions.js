95d19081dd9474051dafc469eda2501a0b0a7033
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-16 23:47:40 +0700

    final fix

--- src/controller/functions.js
+++ src/controller/functions.js
@@ -5,8 +5,6 @@ const config = require('../config')
 const bubble = require('../json/homeworkJSON.json')
 const flexMessage = require('../json/flexTemplate.json')
 
-const safeParseObject = obj => JSON.parse(JSON.stringify(obj))
-
 const generateHomeworkJSON = (arr) => ({
     ...flexMessage,
     altText: "homework",
@@ -38,7 +36,7 @@ const generateAssignments = async (arr, subjectName) => {
         const status = isOverdue
             ? '✅'
             : '(📅 ' + getDeadlineFromDate(new Date(task.deadline)) + ' ' + getLocalTimeFromDate(new Date(task.deadline)) + ')'
-        return '-' + task.name + ': ' + task.link + ' ' + status
+        return '- ' + task.name + ': ' + task.link + ' ' + status
     })
     str = str.join('\n')
     return {
@@ -47,7 +45,6 @@ const generateAssignments = async (arr, subjectName) => {
     }
 }
 
-// Function to sort array of objects by parameter
 const sortByParam = (arr, param) => {
     const arrCopy = [...arr]
     arrCopy.sort((a, b) => {
@@ -59,18 +56,17 @@ const sortByParam = (arr, param) => {
     })
     return arrCopy
 }
-// E.g sort by sortByParam(arr, 'deadline')
 
-// Returns deadline in {(Month) (Day)} format from JS DateTime Object
+// Returns deadline in {(Month) (Day)} format
 const getDeadlineFromDate = (dateTimeObject) => {
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     return monthNames[dateTimeObject.getMonth()] + ' '  + dateTimeObject.getDate()
 }
 
-// Returns local time in HH:MM format from JS DateTime Object
+// Local time string in HH:MM format
 const getLocalTimeFromDate = (dateTimeObject) => moment(dateTimeObject).tz('Asia/Bangkok').format('HH:mm')
 
-// Deep Clone Function
+// Deep Clone
 const clone = (obj) => {
     if (obj === null || typeof obj !== 'object' || 'isActiveClone' in obj) return obj
 
@@ -78,16 +74,15 @@ const clone = (obj) => {
 
     Object.keys(obj).forEach((key) => {
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            obj['isActiveClone'] = null
+            obj.isActiveClone = null
             temp[key] = clone(obj[key])
-            delete obj['isActiveClone']
+            delete obj.isActiveClone
         }
     })
 
     return temp
 }
 
-// Returns array of subjects with assignments sorted by deadline
 const getSubjectAssignmentsSorted = (arr) =>
     arr.map((subject) => {
         const sorted = sortByParam(subject.assignments, 'deadline')
@@ -103,33 +98,30 @@ const generateBubbles = (arr) => {
     const subjects = sortByParam(getSubjectAssignmentsSorted(arr), 'latest')
     return subjects.map((subject) => {
         let bubbleClone = clone(bubble)
-        const displayedDeadline = subject['latest'] ? getDeadlineFromDate(new Date(subject['latest'])) : '-'
+        const displayedDeadline = subject.latest ? getDeadlineFromDate(new Date(subject.latest)) : '-'
         // Set subject title
-        bubbleClone['header']['contents'][0]['text'] = subject['title']
-        bubbleClone['header']['contents'][0]['action']['data'] = `homework/header/${subject['title']}`
+        bubbleClone.header.contents[0].text = subject.title
+        bubbleClone.header.contents[0].action.data = 'homework/header/' + subject.title
 
         // Set subject deadline
-        bubbleClone['hero']['contents'][0]['text'] = '📅 Deadline' + displayedDeadline
-        bubbleClone['hero']['contents'][0]['contents'][0]['text'] = '📅 Deadline: '
-        bubbleClone['hero']['contents'][0]['contents'][1]['text'] = displayedDeadline
-        bubbleClone['hero']['contents'][0]['action']['data'] = `homework/hero/${subject['title']}`
+        bubbleClone.hero.contents[0].text = '📅 Deadline' + displayedDeadline
+        bubbleClone.hero.contents[0].contents[0].text = '📅 Deadline: '
+        bubbleClone.hero.contents[0].contents[1].text = displayedDeadline
+        bubbleClone.hero.contents[0].action.data = 'homework/hero/' + subject.title
 
         // Set post-back
-        bubbleClone['footer']['contents'][0]['action']['data'] = `homework/solution/${subject['title']}`
+        bubbleClone.footer.contents[0].action.data = 'homework/solution/' + subject.title
         return bubbleClone
     })
 }
 
-// Generate a message containing a list of subjects
 const generateSubjectList = (courses) => ({
     type: 'text',
-    text: 'Select from the following:\n' + courses.map((course) => '- ' + course['title']).join('\n'),
+    text: 'Select from the following:\n' + courses.map((course) => '- ' + course.title).join('\n'),
 })
 
-// Gets the local datetime from a UTC datetime
 const getLocalFromUTC = (UTCDateTime) => moment(UTCDateTime).tz('Asia/Bangkok')
 
-// Shorten a URL using bit.ly
 const shortenURL = async (URL) => {
     const response = await request.post({
         uri: 'https://api-ssl.bitly.com/v4/shorten',
@@ -141,16 +133,15 @@ const shortenURL = async (URL) => {
         },
         json: true,
     })
-    return response['link']
+    return response.link
 }
 
-// Get the number of clicks from a shortened links
 const getClicksFromURL = async (URL) => {
     URL = URL.replace(/(^\w+:|^)\/\//, '')
     const response = await request.post({
         uri: `https://api-ssl.bitly.com/v4/bitlinks/${URL}/clicks/summary`,
     })
-    return response['total_clicks']
+    return response.total_clicks
 }
 
 // Function exports
