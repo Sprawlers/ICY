d72c68ac422ac3713b57a8cff96e82e13f9de002
bosswt <boss.wt13@gmail.com>
2020-04-18 16:54:51 +0700

    config update

--- src/config.js
+++ src/config.js
@@ -1,20 +1,21 @@
-const dotenv = require('dotenv')
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const envFound = dotenv.config({ path: './.env' })
-if (!envFound) console.log("⚠️  Couldn't find .env file")
+if (process.env.NODE_ENV === 'development') {
+	const dotenv = require('dotenv')
+	const envFound = dotenv.config({ path: '../.env' })
+	if (!envFound) throw new Error("⚠️  Couldn't find .env file")
+}
 
 module.exports = {
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
-  bitly_token: process.env.BITLY_TOKEN,
-  webhookid: process.env.WEBHOOK_ID,
-  db_host: process.env.DB_HOST,
-  port: process.env.PORT,
-  projectId: process.env.PROJECT_ID,
-  line: {
-    channelAccessToken: process.env.DEV_CHANNEL_TOKEN,
-    channelSecret: process.env.DEV_CHANNEL_SECRET,
-  },
+	client_email: process.env.GOOGLE_CLIENT_EMAIL,
+	private_key: process.env.GOOGLE_PRIVATE_KEY,
+	bitly_token: process.env.BITLY_TOKEN,
+	webhookid: process.env.WEBHOOK_ID,
+	db_host: process.env.DB_HOST,
+	port: process.env.PORT,
+	projectId: process.env.PROJECT_ID,
+	line: {
+		channelAccessToken: process.env.DEV_CHANNEL_TOKEN,
+		channelSecret: process.env.DEV_CHANNEL_SECRET,
+	},
 }
