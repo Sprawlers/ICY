570ac39414caa66eb3e97bcfb55168a8bb73fdd6
bosswt <boss.wt13@gmail.com>
2020-04-18 16:00:31 +0700

    Update path for JSONController

--- src/json/JSONcontroller.js
+++ src/json/JSONcontroller.js
@@ -1,22 +1,22 @@
 const TemplateA = require('./TemplateA.json')
 const TemplateB = require('./TemplateB.json')
-const feedback = require('./feedback.json')
-const feedback_canceled = require('./feedback_canceled.json')
-const feedback_confirm = require('./feedback_confirm.json')
-const feedback_submitted = require('./feedback_submitted.json')
-const regular_message = require('./regular_message.json')
+const feedback = require('./intent/feedback.json')
+const feedback_canceled = require('./intent/feedback_canceled.json')
+const feedback_confirm = require('./intent/feedback_confirm.json')
+const feedback_submitted = require('./intent/feedback_submitted.json')
+const regular_message = require('./intent/regular_message.json')
 
 // Refer to image of each template in images folder
 const dict = {
-    TemplateA,
-    TemplateB,
-    feedback,
-    feedback_canceled,
-    feedback_confirm,
-    feedback_submitted,
-    regular_message
+	TemplateA,
+	TemplateB,
+	feedback,
+	feedback_canceled,
+	feedback_confirm,
+	feedback_submitted,
+	regular_message,
 }
 
 module.exports = {
-    JSONfile: templateName => dict[templateName]
+	JSONfile: (templateName) => dict[templateName],
 }
