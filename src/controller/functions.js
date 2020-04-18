5270d94f140c6968dca606e507b0537f8b59ab5d
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-18 23:14:46 +0700

    fix

--- src/controller/functions.js
+++ src/controller/functions.js
@@ -35,11 +35,11 @@ const generateExamMessageJSON = arr => {
 const generateEachExamsJSON = arr => arr.map(obj => {
     const json = clone(JSONfile('singleExam'))
     let [left, right] = json.contents
-    const duration = (obj.duration === 0)?' ':obj.duration
+    const duration = (obj.duration === 0)?'':' (' + obj.duration + 'm)'
     left.contents[0].text = obj.title
     left.contents[1].text = obj.name
     right.contents[0].text = getDeadlineFromDate(new Date(obj.date))
-    right.contents[1].text = getLocalTimeFromDate(new Date(obj.date)) + ' (' + duration + 'm)'
+    right.contents[1].text = getLocalTimeFromDate(new Date(obj.date)) + duration
     json.contents = [left, right]
     return json
 })
