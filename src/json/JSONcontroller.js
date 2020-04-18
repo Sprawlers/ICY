c681f5fa937687c1d2fcd2cd0a47482eae532019
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-18 15:46:32 +0700

    updated json controller'

--- src/json/JSONcontroller.js
+++ src/json/JSONcontroller.js
@@ -1,10 +1,20 @@
 const TemplateA = require('./TemplateA.json')
 const TemplateB = require('./TemplateB.json')
+const feedback = require('./feedback.json')
+const feedback_cancelled = require('./feedback_cancelled.json')
+const feedback_confirm = require('./feedback_confirm.json')
+const feedback_submitted = require('./feedback_submitted.json')
+const regular_message = require('./regular_message.json')
 
 // Refer to image of each template in images folder
 const dict = {
-    'TemplateA': TemplateA,
-    'TemplateB': TemplateB
+    TemplateA,
+    TemplateB,
+    feedback,
+    feedback_cancelled,
+    feedback_confirm,
+    feedback_submitted,
+    regular_message
 }
 
 module.exports = {
